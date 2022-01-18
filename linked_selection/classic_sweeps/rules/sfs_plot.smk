81af7335edc8582af966cbeeca2f601cc7dc86c3
molpopogen <krthornt@uci.edu>
2022-01-18 12:03:56 -0800

    add table comparing fixation times to simple Python simulation

--- linked_selection/classic_sweeps/rules/sfs_plot.smk
+++ linked_selection/classic_sweeps/rules/sfs_plot.smk
@@ -1,7 +1,7 @@
 rule make_sfs_plot:
     input: "output/data.sqlite3",
            "testcode/sfs_plot.py",
-    output: report("output/sfs.png")
+    output: report("output/sfs.png", category="Allele frequency spectrum")
     threads: 1
     shell:
         """
---
+++ linked_selection/classic_sweeps/testcode/summarise_fixation_times.py
@@ -0,0 +1,20 @@
+import sqlite3
+import dataframe_image
+import pandas as pd
+
+DBNAME = "output/data.sqlite3"
+
+with sqlite3.connect(DBNAME) as conn:
+    fp11 = pd.read_sql("select * from fwdpy11_fixation_times", conn)
+    py = pd.read_sql("select * from python_fixation_times", conn)
+
+    fp11["simulator"] = ["fwdpy11"] * len(fp11.index)
+    py["simulator"] = ["python"] * len(py.index)
+
+    df = pd.concat([fp11, py])
+
+    df = df.groupby(["simulator", "alpha"]).describe() #.reset_index()
+
+    dataframe_image.export(
+        df, "output/fixation_times.png", table_conversion="matplotlib"
+    )
